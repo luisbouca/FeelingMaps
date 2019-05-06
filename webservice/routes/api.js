1c2a2cfb0e0fbf0469ac5c707bae4b961e3e78f4
Luis Bouça <luismrbouca@gmail.com>
2019-05-06 14:19:27 +0100

    Session managment, comments made by other users display, show my comment editable if already commented

--- webservice/routes/api.js
+++ webservice/routes/api.js
@@ -51,8 +51,9 @@ router.get('/Level0/:level0/Level1/:level1', function(req, res, next) {
 	});
 });
 
-router.get("/Comments/:cityid/:id/All",(req,res,next)=>{
-	query="SELECT * from classificacao where idCidadeZona=(Select id from cidade_zona where idCidade='"+req.params.cityid+"' and idZona='"+req.params.id+"')"
+router.get("/Comments/:cityId/:zoneId/All",(req,res,next)=>{
+	query="SELECT user.name, classificacao.id, user.email, classificacao.comentario, classificacao.classificacao, GROUP_CONCAT(categoria.nome) as categoria from classificacao INNER JOIN cidade_zona ON classificacao.idCidadeZona = cidade_zona.id INNER JOIN user ON classificacao.idUser = user.id INNER JOIN categoria_classificacao ON classificacao.id = categoria_classificacao.idClassificacao INNER JOIN categoria ON categoria.idCategoria = categoria_classificacao.idCategoria WHERE cidade_zona.idCidade='"+req.params.cityId+"' and cidade_zona.idZona = '"+req.params.zoneId+"' GROUP BY classificacao.id"
+
 	res.locals.connection.query(query, function (error, results, fields) {
 		if (error) throw error;
 		if(JSON.stringify(results) != "[]"){
@@ -98,9 +99,11 @@ router.get("/City/:cityId/:numZones",(req,res,next)=>{
 			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
 		}else{
 			console.log(results)
-			query = "Insert into cidade_zona(idZona, idCidade) Values(0,"+req.params.cityId+")"
-			for(var i = 1; i<req.params.numZones;i++){
-				query += ",("+i+","+req.params.cityId+")"
+			if(req.params.numZones>0){
+				query = "Insert into cidade_zona(idZona, idCidade) Values(0,"+req.params.cityId+")"
+				for(var i = 1; i<req.params.numZones;i++){
+					query += ",("+i+","+req.params.cityId+")"
+				}
 			}
 			res.locals.connection.query(query, function (error, results, fields) {
 				if (error) throw error;
@@ -170,6 +173,71 @@ router.post("/Comments/",(req,res,next)=>{
 	});
 });
 
+router.post("/Comments/Update",(req,res,next)=>{
+
+	console.log(req.body)
+	query="Update classificacao SET comentario='"+req.body.comment+"', classificacao ='"+req.body.rating+"' where id="+req.body.idComment
+	res.locals.connection.query(query, function (error, results, fields) {
+		if (error) throw error;
+		if(JSON.stringify(results) != "[]"){
+			console.log(query)
+			query = "Delete from categoria_classificacao where idClassificacao="+req.body.idComment
+			res.locals.connection.query(query, function (error, results, fields) {
+				if (error) throw error;
+				if(JSON.stringify(results) != "[]"){
+					console.log(query)
+					categories = req.body.categories.split(",")
+					if(categories.length>0){
+						if(categories.length == 1){
+							query = "Select idCategoria from categoria where nome in('"+categories[0]+"')"
+						}else{
+							query = "Select idCategoria from categoria where nome in('"+categories[0]+"'"
+							for(var i = 1; i<categories.length;i++){
+								query += ",'"+categories[i]+"'"
+							}
+							query += ")"
+						}
+						res.locals.connection.query(query, function (error, results, fields) {
+							if (error) throw error;
+							if(JSON.stringify(results) != "[]"){
+								console.log(query)
+								
+								if(results.length>0){
+									if(results.length == 1){
+										query = "Insert Into categoria_classificacao (idCategoria,idClassificacao) Values("+results[0].idCategoria+","+req.body.idComment+")"
+									}else{
+										query = "Insert Into categoria_classificacao (idCategoria,idClassificacao) Values("+results[0].idCategoria+","+req.body.idComment+")"
+										for(var i = 1; i<results.length;i++){
+											query += ",("+results[i].idCategoria+","+req.body.idComment+")"
+										}
+									}
+									res.locals.connection.query(query, function (error, results, fields) {
+										if (error) throw error;
+										if(JSON.stringify(results) != "[]"){
+											console.log(query)
+											res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+										}else{
+											res.send(JSON.stringify({"status": 200, "error": null, "response": "Not Found"}))
+										}
+									});
+									
+								}
+								
+							}else{
+								res.send(JSON.stringify({"status": 200, "error": null, "response": "Not Found"}))
+							}
+						});
+					}
+				}else{
+					res.send(JSON.stringify({"status": 200, "error": null, "response": "Not Found"}))
+				}
+			});
+		}else{
+			res.send(JSON.stringify({"status": 200, "error": null, "response": "Not Found"}))
+		}
+	});
+});
+
 router.get('/Name/:name/Email/:email/Password/:password', function(req, res, next) {
 	query="INSERT INTO user (name, email, password) VALUES('"+req.params.name+"', '"+req.params.email+"', '"+req.params.password+"')"
 
@@ -183,5 +251,6 @@ router.get('/Name/:name/Email/:email/Password/:password', function(req, res, nex
 		}
 	});
 });
+
 
 module.exports = router;
